43a133bf24d0c45ecb96ac4c944a5a2aefea4dd1
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-05-16 20:52:53 +0900

    finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_085524) do
+ActiveRecord::Schema.define(version: 2017_03_07_140030) do
 
   create_table "books", force: :cascade do |t|
     t.text "body"
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
   end
 
 end
